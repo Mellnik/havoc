86c82076123347b464deb0a6b1de37df294255ed
Omaha <omaha@vmpmod.com>
2015-01-25 15:23:44 +0100

    Ban page fixes

--- website/home/pages/bans.php
+++ website/home/pages/bans.php
@@ -16,7 +16,7 @@
 			</tr>
 			
 			<?php
-			
+					
 				$query = $mysqli->query("SELECT * FROM `bans` ORDER BY `Date` DESC LIMIT 50;");
 				$bans = $query->num_rows;
 				
@@ -57,7 +57,7 @@
 		}
 		else
 		{
-			$stmt = $mysqli->prepare("SELECT * FROM bans WHERE PlayerName = ?;");
+			$stmt = $mysqli->prepare("SELECT `id` FROM `accounts` WHERE `name` = ?;");
 			
 			$stmt->bind_param("s", $player);
 			$stmt->execute();
@@ -65,38 +65,62 @@
 			
 			if($stmt->num_rows == 0)
 			{
-				msg("Player is not banned.");
+				msg("Player not found or invalid.");
 				
 				$stmt->free_result();
 				$stmt->close();
 			}
 			else
 			{
-				$stmt->bind_result($id, $playername, $adminname, $reason, $lift, $date);
+				$stmt->bind_result($id);
 				$stmt->fetch();
 				$stmt->free_result();
 				$stmt->close();
 				
-				?>
-				<font face="Oswald" size="20"><?php echo "$player"; ?></font>
-				<table class="neftable" style="min-width: 100%;">
-					<tr>
-						<th colspan="3">Information</th>
-					</tr>
-					<tr>
-				<?php
-				$display = $lift == 0 ? "Permanent" : date("d.m.Y H:i:s", $lift);
-				echo "<tr><td>Ban ID:</td><td width='70%'>$id</td></tr>";
-				echo "<tr><td>Player profile:</td><td width='70%'><a class='neflink' href='/players/$playername'>$playername</a></td></tr>";
-				echo "<tr><td>Banned by:</td><td width='70%'>$adminname</td></tr>";
-				echo "<tr><td>Reason:</td><td width='70%'>$reason</td></tr>";
-				echo "<tr><td>Expires:</td><td width='70%'>$display</td></tr>";
-				echo "<tr><td>Banned on:</td><td width='70%'>".date("d.m.Y H:i:s", $date)."</td></tr>";
+				$q = $mysqli->query("SELECT accounts.name, bans.* FROM bans LEFT JOIN accounts ON accounts.id = bans.admin_id WHERE bans.id = $id;");
 				
-				?>
-					</tr>
-				</table>
-				<?php
+				if($q->num_rows == 0)
+				{
+					msg("Player was not banned.");
+				}
+				else
+				{	
+					$r = $q->fetch_assoc();
+					
+					$bans = array();
+					$bans['admin'] = $r['name'];
+					$bans['reason'] = $r['reason'];
+					$bans['lift'] = $r['lift'];
+					$bans['date'] = $r['date'];
+					?>
+					<font face="Oswald" size="20"><?php echo "$player"; ?></font>
+					<table class="havoctable" style="min-width: 100%;">
+						<tr>
+							<th colspan="3">Information</th>
+						</tr>
+						<tr>
+					<?php
+					$display = $bans['lift'] == 0 ? "Permanent" : date("d.m.Y H:i:s", $bans['lift']);
+					echo "<tr><td>Ban ID:</td><td width='70%'>$id</td></tr>";
+					echo "<tr><td>Player profile:</td><td width='70%'><a class='neflink' href='/players/$player'>$player</a></td></tr>";
+					echo "<tr><td>Banned by:</td><td width='70%'><a class='neflink' href='/players/".$bans['admin']."'>".$bans['admin']."</a></td></tr>";
+					echo "<tr><td>Reason:</td><td width='70%'>".$bans['reason']."</td></tr>";
+					echo "<tr><td>Expires:</td><td width='70%'>$display</td></tr>";
+					echo "<tr><td>Banned on:</td><td width='70%'>".date("d.m.Y H:i:s", $bans['date'])."</td></tr>";
+					echo "<tr><td>Ping:</td><td width='70%'>".$r['ping']."</td></tr>";
+					echo "<tr><td>Position:</td><td width='70%'>".$r['xpos']." ".$r['ypos']." ".$r['zpos']."</td></tr>";
+					echo "<tr><td>Packetloss:</td><td width='70%'>".$r['packetloss']."%</td></tr>";
+					echo "<tr><td>State:</td><td width='70%'>".$r['state']."</td></tr>";
+					echo "<tr><td>Health:</td><td width='70%'>".$r['health']."%</td></tr>";
+					echo "<tr><td>Armor:</td><td width='70%'>".$r['armor']."%</td></tr>";
+					echo "<tr><td>God:</td><td width='70%'>".$r['god']."</td></tr>";
+					echo "<tr><td>Weapon:</td><td width='70%'>".$r['weapon']."</td></tr>";
+					
+					?>
+						</tr>
+					</table>
+					<?php
+				}
 			}
 		}
 	}
