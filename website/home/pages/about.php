4949e98bcfcb9ad43d737e4d6f4587ed5f5bed45
Omaha <omaha@vmpmod.com>
2015-03-15 12:25:43 +0100

    Website fixes

--- website/home/pages/about.php
+++ website/home/pages/about.php
@@ -9,7 +9,7 @@
 					New Evolution Freeroam (NEF) is primarily a multiplayer server for GTA:San Andreas Multiplayer (<a class="neflink" href="http://sa-mp.com">http://sa-mp.com</a>).
 					<br><br>
 					If you like to burn that rubber in a race, blast those bullets in deathmatch, pump the adrenaline out of your body with stunting or just want to hang around, then this server suits you well!
-					NEF is requently being updated with a lots of new features and maps, so who said San Andreas gets old?
+					NEF is frequently being updated with a lots of new features and maps, so who said San Andreas gets old?
 					<br><br>
 					Enjoy playing at NEF!
 					</span>
